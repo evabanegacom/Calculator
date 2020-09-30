d5aa9326b57ff5820542e1a938805d26bd778804
Precious chiedu <udegbue69@gmail.com>
2020-09-30 09:08:17 +0100

    changed button color

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -4,32 +4,32 @@ import Button from './button';
 const ButtonPanel = () => (
   <div className="button-collection">
     <div className="button-group">
-      <Button name="AC" color="#E0E0E0" />
-      <Button name="+/-" />
-      <Button name="%" />
+      <Button name="AC" color='colors' />
+      <Button name="+/-" color='colors' />
+      <Button name="%" color='colors' />
       <Button name="÷" />
     </div>
     <div className="button-group">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" color='colors' />
+      <Button name="8" color='colors' />
+      <Button name="9" color='colors' />
       <Button name="X" />
     </div>
     <div className="button-group">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" color='colors' />
+      <Button name="5" color='colors' />
+      <Button name="6" color='colors' />
       <Button name="-" />
     </div>
     <div className="button-group">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" color='colors' />
+      <Button name="2" color='colors' />
+      <Button name="3" color='colors' />
       <Button name="+" />
     </div>
     <div className="button-group last-btns">
-      <Button name="0" />
-      <Button name="." />
+      <Button name="0" color='colors' />
+      <Button name="." color='colors' />
       <Button name="=" />
     </div>
   </div>
